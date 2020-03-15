36bc1df80d68d9cbb8eaf3e163f947bfdfdd7cc7
jengelmo <jengelmo@tuenti.com>
2020-03-15 21:45:49 +0100

    First commit with some first intents

---
+++ README.md
@@ -0,0 +1,2 @@
+# Alexa Skill: Aplana la curva 
+Skill de la iniciativa 'Aplana la curva' para la lucha contra el Covid-19
---
+++ lambda/custom/languageStrings.js
@@ -0,0 +1,22 @@
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: '¡Hola! Gracias por tu interés en informarte mejor sobre el nuevo coronavirus. Estoy aquí para intentar ayudarte en todo lo que necesites y darte toda la información que me pidas y que esté en mis manos.',
+            WHAT_IS_COD19_MSG: 'Esa es la primera respuesta que hay que saber. Te cuento: realmente, los coronavirus son una familia de virus que, normalmente, solo afectan a animales.',
+            SYMPTOMS_MSG: 'Seguramente ya hayas leído algo, pero te resumo: lo más común y los síntomas más leves incluyen tos, fiebre y sensación de falta de aire. En algunos casos también afecta al sistema digestivo con diarrea y dolor abdominal. \n' +
+                ' \n' +
+                ' En los casos de mayor gravedad, la infección puede causar neumonía, dificultades para respirar, fallo renal y la muerte. Los casos más graves normalmente ocurren en personas de edad avanzada, que padecen alguna enfermedad del corazón, del pulmón o problemas en el sistema inmunitario.\n' +
+                ' \n' +
+                ' Por favor, contacta con tu médico si presentas cualquiera de estos síntomas.',
+            TRAVELING_MSG: 'Es un tema complejo. Evita, en la medida de lo posible, hacer viajes, ya que de alguna forma puedes contribuir a la expansión del virus. \n' +
+                ' \n' +
+                ' Sigue todas las recomendaciones y únete al movimiento yo me quedo en casa para frenar la curva y ayudar al sistema sanitario a no enfrentarse a situaciones de colapso.',
+            TEST_MSG: 'Normalmente, las pruebas consisten en extraer muestras de la garganta o de la nariz con un bastoncillo.',
+            HELP_MSG: 'Puedes decirme hola. Cómo te puedo ayudar?',
+            GOODBYE_MSG: 'Hasta luego!',
+            REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+            ERROR_MSG: 'Lo siento, ha habido un error. Por favor inténtalo otra vez.'
+        }
+    }
+}
